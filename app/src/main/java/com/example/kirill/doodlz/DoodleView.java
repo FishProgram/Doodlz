3ec3a4cbff05c15c599b1313dbddd115a5ac0f70
Sergeev Kirill <Sergeev.Kirill.S@yandex.ru>
2018-02-13 18:27:11 +0400

    init commit

---
+++ app/src/main/java/com/example/kirill/doodlz/DoodleView.java
@@ -0,0 +1,16 @@
+package com.example.kirill.doodlz;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Kirill on 13.02.2018.
+ */
+
+public class DoodleView extends View {
+    public DoodleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/kirill/doodlz/LineWidthDialogFragment.java
@@ -0,0 +1,8 @@
+package com.example.kirill.doodlz;
+
+/**
+ * Created by Kirill on 13.02.2018.
+ */
+
+public class LineWidthDialogFragment {
+}
